bf7b5024c6ed12475110ba6241c8dfff6c3b67e5
tristanaristanto <tristantriplefive@gmail.com>
2025-11-23 15:31:57 +0700

    Update simulasi.py

--- simulasi.py
+++ simulasi.py
@@ -5,7 +5,7 @@ import random
 
 # --- KONFIGURASI ---
 THINGSBOARD_HOST = 'thingsboard.cloud'
-ACCESS_TOKEN = ''
+ACCESS_TOKEN = 'WKikT5qF9eWzjmXZJc2G'
 # Setup MQTT Client
 client = mqtt.Client()
 client.username_pw_set(ACCESS_TOKEN)
